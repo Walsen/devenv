1bbc75a8fc4c538bd50604edfa95ad02c2359a1e
Sergio D. Rodríguez Inclán <srinclan@gmail.com>
2021-01-13 18:42:07 -0400

    Install AWSCli, kubectl and krew.

---
+++ ansible/files/installKrew.sh
@@ -0,0 +1,8 @@
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
+  "$KREW" install krew
+)
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> $HOME/.zshrc
